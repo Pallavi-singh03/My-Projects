a603d1690de1dd28555361977ba3fe941b8105f6
Pallavi Singh <149757850+Pallavi-singh03@users.noreply.github.com>
2025-07-03 14:42:25 +0530

    Update addnum.java

--- addnum.java
+++ addnum.java
@@ -1,18 +1,11 @@
-/**
- * 
- */
-package Shubham.javaproject;
-
-/**
- * 
- */
+import java.util.Scanner;
 public class addnum {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int sum = a + b;
+		System.out.println(sum);
 
 	}
 
